e9f073d756d3df6d5735bb101b57b99c5468e44d
Hiren Joshi <hirenj@gmail.com>
2016-04-04 23:14:24 +0200

    Add empty template for merging

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,18 @@ module.exports = function(grunt) {
 	});
 
 	grunt.registerTask('build_cloudformation', 'Build cloudformation template',function() {
-		var template_paths = grunt.file.expand('node_modules/*/**/resources/*.template');
+		var template_paths = ['empty.template'];
+		template_paths = template_paths.concat(grunt.file.expand('node_modules/*/**/resources/*.template'));
+		template_paths = template_paths.concat(grunt.file.expand('resources/*.template'));
 		var templates = template_paths.map(function(template) {
 			return grunt.file.readJSON(template);
 		});
 		var common_template = templates.reduce(function(prev,curr) {
 			if (! prev) {
 				return curr;
+			}
+			if ( ! prev.Resources ) {
+				prev.Resources = {};
 			}
 			Object.keys(curr.Resources).forEach(function(key) {
 				prev.Resources[key] = curr.Resources[key];
@@ -28,6 +33,6 @@ module.exports = function(grunt) {
 			});
 			return prev;
 		});
-		grunt.file.write('resources/glycodomain.template',JSON.stringify(common_template,null,'  '));
+		grunt.file.write('glycodomain.template',JSON.stringify(common_template,null,'  '));
 	});
 };
