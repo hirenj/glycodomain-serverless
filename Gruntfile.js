65493b0b1938cd3e009d17a845121fd4a195efc6
Hiren Joshi <hirenj@gmail.com>
2016-04-07 22:12:26 +0200

    Add reading of resources from cloudformation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,74 @@
 /**
  */
 'use strict';
+
+require('es6-promise').polyfill();
+var AWS = require('aws-sdk');
+
+AWS.Request.prototype.promise = function() {
+  return new Promise(function(accept, reject) {
+    this.on('complete', function(response) {
+      if (response.error) {
+        reject(response.error);
+      } else {
+        accept(response);
+      }
+    });
+    this.send();
+  }.bind(this));
+};
+
+var read_stack_resources = function(stack) {
+	var cloudformation = new AWS.CloudFormation({region:'us-east-1'});
+	return cloudformation.listStackResources({ 'StackName' : stack }).promise().then(function(resources) {
+		return resources.data.StackResourceSummaries;
+		console.log();
+	}).catch(function(err) {
+		console.log(err);
+		console.log(err.stack);
+		return Promise.resolve(true);
+	});
+};
+
+var make_lookup = function(resources) {
+	var result = {};
+	resources.forEach(function(resource) {
+		result[resource.LogicalResourceId] = resource.PhysicalResourceId;
+	});
+	return result;
+};
+
+var summarise_resources = function(stack,resources) {
+	var lambdas = resources.filter(function(resource) {
+		return resource.ResourceType == 'AWS::Lambda::Function';
+	});
+	var dynamodbs = resources.filter(function(resource) {
+		return resource.ResourceType == 'AWS::DynamoDB::Table';
+	});
+	var buckets = resources.filter(function(resource) {
+		return resource.ResourceType == 'AWS::S3::Bucket';
+	});
+	var stack_conf = { 	'functions' : make_lookup(lambdas),
+						'tables' : make_lookup(dynamodbs),
+						'buckets' : make_lookup(buckets) };
+	return stack_conf;
+}
+
 module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	var path = require('path');
 	grunt.initConfig({
+	});
+
+	grunt.registerTask('get_resources', 'Get CloudFormation resources', function(stack) {
+		var done = this.async();
+		stack = stack || 'test';
+		read_stack_resources(stack).then(function(resources) {
+			var summary = summarise_resources(stack,resources);
+			grunt.file.write(stack+'-resources.conf.json',JSON.stringify(summary,null,'  '));
+			done();
+		});
 	});
 
 	grunt.registerTask('build_cloudformation', 'Build cloudformation template',function() {
