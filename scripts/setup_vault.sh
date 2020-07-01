37c683dd0d86651bb35ae71bd5f8afdf61bf00b7
Hiren Joshi <hirenj@gmail.com>
2020-07-01 10:21:32 +0200

    More sophisticated setup for adding credentials to vault

--- scripts/setup_vault.sh
+++ scripts/setup_vault.sh
@@ -1,14 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-stage="${1:-gator-test}"
-credentials_file="$2"
-region="us-east-1"
-role="CreateChangeSet"
+application="${1:-gator-test}"
+role="${2:-CreateChangeSet}"
+AWS_REGION="${AWS_REGION:-us-east-1}"
+credentials_file="$4"
 policies="... READ FROM BUILD DUMP .." #Comma separated
 
-vault_path="aws-gator-${stage}"
+vault_path="aws-${application}"
 
-vault secrets enable -path="$vault_path" aws
-vault write "$vault_path/config/lease" lease=15m lease_max=1h
-#vault write "$vault_path/config/root" access_key="" secret_key="" region="$region"
-vault write "$vault_path/roles/$role" policy_arns=$policies credential_type=iam_user
+engine_exists=$(vault secrets list | grep "$vault_path")
+
+if [[ "$engine_exists" == "" ]]; then
+	vault secrets enable -path="$vault_path" aws
+	vault write "$vault_path/config/lease" lease=10m lease_max=1h
+fi
+
+if [[ "$credentials_file" != "" ]]; then
+
+	while IFS="," read -r access_key_id secret_access_key
+	do
+	ACCESS_KEY_ID="$access_key_id"
+	secret_access_key="${secret_access_key%$'\n'}"
+	secret_access_key="${secret_access_key%$'\r'}"
+	SECRET_ACCESS_KEY="$secret_access_key"
+	done < "$credentials_file"
+
+	vault write "$vault_path/config/root" access_key="$ACCESS_KEY_ID" secret_key="$SECRET_ACCESS_KEY" region="$AWS_REGION"
+fi
+
+#vault write "$vault_path/roles/$role" policy_arns=$policies credential_type=iam_user
+
+# Test reading the config
+
+echo "Testing reading an Access key from the vault at $vault_path for role $role"
+vault read -format=json -field=access_key "${vault_path}/creds/${role}"
